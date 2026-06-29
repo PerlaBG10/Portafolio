19e17c227285b293cd641842e71de4c3131653d5
PerlaBG10 <cristalbg10@gmail.com>
2026-06-29 16:14:06 +0200

    Página web sobre peluquería, proyecto personal parte Frontend

---
+++ Páginaweb_StyleHair/js/script.js
@@ -0,0 +1,7 @@
+function botonEnviar() {
+    alert("¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.");
+}
+
+function botonCancelar() {
+    alert("Has cancelado el envío del mensaje.");
+}
